d18e4726c680cc22b93438ef711fa3dc2bd7f55d
SohanKafle <soka_bca2079@lict.edu.np>
2025-03-08 21:27:56 +0545

    loop

---
+++ loop.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Example of a for loop
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Iteration number: " + i);
+        }
+    }
+}
