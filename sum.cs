f23a97fb1ff8b63ccb9d9d4a2db9d4762c39eca3
SohanKafle <soka_bca2079@lict.edu.np>
2025-03-23 22:28:42 +0545

    commit

---
+++ sum.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+    }
+}
