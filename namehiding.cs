2858b7758192568f1d40107830cc19680e74a102
SohanKafle <soka_bca2079@lict.edu.np>
2025-04-13 20:46:09 +0545

    commit

---
+++ namehiding.cs
@@ -0,0 +1,19 @@
+using static System.Console;
+ 
+ 
+ class Program{
+ 
+   void name(){
+     WriteLine("My Name is Sohan Kafle")
+   }
+ 
+ 
+   static void Main(){
+ 
+     Program p=new Program();
+ 
+     p.name();
+   }
+ 
+ 
+ }
